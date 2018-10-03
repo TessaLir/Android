1c2676eb053e6fb69f13a934259916884ba1040b
VVetukov <finik001@gmail.com>
2018-10-03 09:26:18 +0300

    change mainActivity for events button click

--- Calculator/app/src/main/java/ru/home/vetukov/test/calculator/CalculatorActivity.java
+++ Calculator/app/src/main/java/ru/home/vetukov/test/calculator/CalculatorActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -17,14 +18,21 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathFactory;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    ButtonHelper buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
+        buttons = new ButtonHelper(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        buttons.onClick(v);
     }
 }
