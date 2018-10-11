86902383a0276c1a28df43ac311b66671bffb1d9
VVetukov <finik001@gmail.com>
2018-10-11 12:14:16 +0300

    Add Service for Single query

---
+++ MyWeatherApp/app/src/main/java/ru/vetukov/weather/myweatherapp/WeatherService.java
@@ -0,0 +1,30 @@
+package ru.vetukov.weather.myweatherapp;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import ru.vetukov.weather.myweatherapp.objects.Example;
+
+/*
+http://api.openweathermap.org
+    /data/2.5/weather
+        q=Moscow
+        units=metric
+        lang=ru
+        appid=c7d271abe99645d6f5ca56a562688c84
+*/
+
+public interface WeatherService {
+
+    // запрос на текущую температуру.
+    @GET("/data/2.5/weather")
+    Call<Example> getWeatherNow (
+            @Query("q") String q,
+            @Query("units") String units,
+            @Query("lang") String lang,
+            @Query("appid") String appid
+    );
+
+
+}
