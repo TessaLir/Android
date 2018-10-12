72470cc7541501c1cc03fadf1caa86ce5c01c759
VVetukov <finik001@gmail.com>
2018-10-12 09:45:49 +0300

    removed unnecessary data

--- MyWeatherApp/app/src/main/java/ru/vetukov/weather/myweatherapp/objects/SingleSearchWeatherObj.java
+++ MyWeatherApp/app/src/main/java/ru/vetukov/weather/myweatherapp/objects/SingleSearchWeatherObj.java
@@ -6,9 +6,6 @@ import com.google.gson.annotations.SerializedName;
 
 public class SingleSearchWeatherObj {
 
-    @SerializedName("coord")
-    @Expose
-    private Coord coord;
     @SerializedName("weather")
     @Expose
     private List<Weather> weather = null;
@@ -21,18 +18,9 @@ public class SingleSearchWeatherObj {
     @SerializedName("visibility")
     @Expose
     private Integer visibility;
-    @SerializedName("wind")
-    @Expose
-    private Wind wind;
-    @SerializedName("clouds")
-    @Expose
-    private Clouds clouds;
     @SerializedName("dt")
     @Expose
     private Integer dt;
-    @SerializedName("sys")
-    @Expose
-    private Sys sys;
     @SerializedName("id")
     @Expose
     private Integer id;
@@ -42,14 +30,6 @@ public class SingleSearchWeatherObj {
     @SerializedName("cod")
     @Expose
     private Integer cod;
-
-    public Coord getCoord() {
-        return coord;
-    }
-
-    public void setCoord(Coord coord) {
-        this.coord = coord;
-    }
 
     public List<Weather> getWeather() {
         return weather;
@@ -83,36 +63,12 @@ public class SingleSearchWeatherObj {
         this.visibility = visibility;
     }
 
-    public Wind getWind() {
-        return wind;
-    }
-
-    public void setWind(Wind wind) {
-        this.wind = wind;
-    }
-
-    public Clouds getClouds() {
-        return clouds;
-    }
-
-    public void setClouds(Clouds clouds) {
-        this.clouds = clouds;
-    }
-
     public Integer getDt() {
         return dt;
     }
 
     public void setDt(Integer dt) {
         this.dt = dt;
-    }
-
-    public Sys getSys() {
-        return sys;
-    }
-
-    public void setSys(Sys sys) {
-        this.sys = sys;
     }
 
     public Integer getId() {
@@ -140,60 +96,11 @@ public class SingleSearchWeatherObj {
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
-    public class Clouds {
-
-        @SerializedName("all")
-        @Expose
-        private Integer all;
-
-        public Integer getAll() {
-            return all;
-        }
-
-        public void setAll(Integer all) {
-            this.all = all;
-        }
-
-    }
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    public class Coord {
-
-        @SerializedName("lon")
-        @Expose
-        private Double lon;
-        @SerializedName("lat")
-        @Expose
-        private Double lat;
-
-        public Double getLon() {
-            return lon;
-        }
-
-        public void setLon(Double lon) {
-            this.lon = lon;
-        }
-
-        public Double getLat() {
-            return lat;
-        }
-
-        public void setLat(Double lat) {
-            this.lat = lat;
-        }
-
-    }
-    ///////////////////////////////////////////////////////////////////////////////////////////////
     public class Main {
 
         @SerializedName("temp")
         @Expose
         private Double temp;
-        @SerializedName("pressure")
-        @Expose
-        private Integer pressure;
-        @SerializedName("humidity")
-        @Expose
-        private Integer humidity;
         @SerializedName("temp_min")
         @Expose
         private Integer tempMin;
@@ -207,22 +114,6 @@ public class SingleSearchWeatherObj {
 
         public void setTemp(Double temp) {
             this.temp = temp;
-        }
-
-        public Integer getPressure() {
-            return pressure;
-        }
-
-        public void setPressure(Integer pressure) {
-            this.pressure = pressure;
-        }
-
-        public Integer getHumidity() {
-            return humidity;
-        }
-
-        public void setHumidity(Integer humidity) {
-            this.humidity = humidity;
         }
 
         public Integer getTempMin() {
@@ -239,77 +130,6 @@ public class SingleSearchWeatherObj {
 
         public void setTempMax(Integer tempMax) {
             this.tempMax = tempMax;
-        }
-
-    }
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    public class Sys {
-
-        @SerializedName("type")
-        @Expose
-        private Integer type;
-        @SerializedName("id")
-        @Expose
-        private Integer id;
-        @SerializedName("message")
-        @Expose
-        private Double message;
-        @SerializedName("country")
-        @Expose
-        private String country;
-        @SerializedName("sunrise")
-        @Expose
-        private Integer sunrise;
-        @SerializedName("sunset")
-        @Expose
-        private Integer sunset;
-
-        public Integer getType() {
-            return type;
-        }
-
-        public void setType(Integer type) {
-            this.type = type;
-        }
-
-        public Integer getId() {
-            return id;
-        }
-
-        public void setId(Integer id) {
-            this.id = id;
-        }
-
-        public Double getMessage() {
-            return message;
-        }
-
-        public void setMessage(Double message) {
-            this.message = message;
-        }
-
-        public String getCountry() {
-            return country;
-        }
-
-        public void setCountry(String country) {
-            this.country = country;
-        }
-
-        public Integer getSunrise() {
-            return sunrise;
-        }
-
-        public void setSunrise(Integer sunrise) {
-            this.sunrise = sunrise;
-        }
-
-        public Integer getSunset() {
-            return sunset;
-        }
-
-        public void setSunset(Integer sunset) {
-            this.sunset = sunset;
         }
 
     }
@@ -359,33 +179,6 @@ public class SingleSearchWeatherObj {
 
         public void setIcon(String icon) {
             this.icon = icon;
-        }
-
-    }
-    ///////////////////////////////////////////////////////////////////////////////////////////////
-    public class Wind {
-
-        @SerializedName("speed")
-        @Expose
-        private Integer speed;
-        @SerializedName("deg")
-        @Expose
-        private Integer deg;
-
-        public Integer getSpeed() {
-            return speed;
-        }
-
-        public void setSpeed(Integer speed) {
-            this.speed = speed;
-        }
-
-        public Integer getDeg() {
-            return deg;
-        }
-
-        public void setDeg(Integer deg) {
-            this.deg = deg;
         }
 
     }
