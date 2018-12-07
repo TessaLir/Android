bc791a3a18b304dbf5b4d80006900d60ae36e14c
VVetukov <finik001@gmail.com>
2018-12-07 21:05:07 +0300

    change main activity

--- MyCinemaViewers/app/src/main/java/ru/vetukov/cinema/mycinemaviewers/MainActivity.java
+++ MyCinemaViewers/app/src/main/java/ru/vetukov/cinema/mycinemaviewers/MainActivity.java
@@ -2,12 +2,46 @@ package ru.vetukov.cinema.mycinemaviewers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import ru.vetukov.cinema.mycinemaviewers.adapters.CinemaAdapter;
+import ru.vetukov.cinema.mycinemaviewers.objects.Film;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Film> films;
+
+    private RecyclerView recycler;
+    private CinemaAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        films = new ArrayList(){};
+
+        for (int i = 1; i < 15; i++) {
+            films.add(new Film("Гарри Поттер " + i
+                    , "Harry Potter " + i
+                    , "9.5"
+                    , 5));
+        }
+
+        recycler = findViewById(R.id.view_filmlist);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recycler.getContext(),
+                linearLayoutManager.getOrientation());
+        recycler.addItemDecoration(dividerItemDecoration);
+
+        recycler.setLayoutManager(linearLayoutManager);
+        adapter = new CinemaAdapter(films);
+
+        recycler.setAdapter(adapter);
     }
 }
