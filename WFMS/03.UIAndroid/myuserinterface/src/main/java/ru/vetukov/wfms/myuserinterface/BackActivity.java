c106294149c0ff523eb9910c46de590da912cd8b
Valentin <finik001@gmail.com>
2019-11-17 20:33:44 +0300

    Разобрано задание 4, в 3 разделе. Intent Взаимодействие между Activity

--- WFMS/03.UIAndroid/myuserinterface/src/main/java/ru/vetukov/wfms/myuserinterface/BackActivity.java
+++ WFMS/03.UIAndroid/myuserinterface/src/main/java/ru/vetukov/wfms/myuserinterface/BackActivity.java
@@ -2,13 +2,42 @@ package ru.vetukov.wfms.myuserinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class BackActivity extends AppCompatActivity {
+public class BackActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_back);
+
+        Double d1 = 0.0;
+        Double d2 = 0.0;
+        Double result = 0.0;
+
+        Intent intent = getIntent();
+
+        if (intent != null) {
+            d1 = Double.parseDouble(intent.getStringExtra("ONE"));
+            d2 = Double.parseDouble(intent.getStringExtra("TWO"));
+            result = d1 + d2;
+        }
+
+        TextView tvResult = findViewById(R.id.main_tv_result);
+        Button btnReturn = findViewById(R.id.main_btn_back);
+
+        tvResult.setText(String.format("%s + %s = %s", d1, d2, result.toString()));
+        btnReturn.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        onBackPressed();
+    }
+
 }
--- WFMS/03.UIAndroid/myuserinterface/src/main/java/ru/vetukov/wfms/myuserinterface/MainActivity.java
+++ WFMS/03.UIAndroid/myuserinterface/src/main/java/ru/vetukov/wfms/myuserinterface/MainActivity.java
@@ -2,14 +2,44 @@ package ru.vetukov.wfms.myuserinterface;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String TAG = "$#@%@ MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.main_btn_button);
+
+        b.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View v) {
+        EditText e1 = findViewById(R.id.main_et_one);
+        EditText e2 = findViewById(R.id.main_et_two);
+
+        String n1 = e1.getText().toString();
+        String n2 = e2.getText().toString();
+
+        Log.d(TAG, String.format("Введены: %s и %s", n1, n2));
+
+        Intent intent = new Intent(this, BackActivity.class);
+
+        intent.putExtra("ONE", n1);
+        intent.putExtra("TWO", n2);
+
+        startActivity(intent);
+
+    }
 }
