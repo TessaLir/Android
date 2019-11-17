fea9c51d571647b4f9b2d520ec18c7d61ecea2ab
Valentin <finik001@gmail.com>
2019-11-17 20:11:26 +0300

    Разобрано задание 3, в 3 разделе.

---
+++ WFMS/03.UIAndroid/myuserinterface/src/main/java/ru/vetukov/wfms/myuserinterface/BackActivity.java
@@ -0,0 +1,14 @@
+package ru.vetukov.wfms.myuserinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_back);
+    }
+}
