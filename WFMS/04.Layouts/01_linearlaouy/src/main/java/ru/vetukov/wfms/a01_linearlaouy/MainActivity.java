292f9ac813649fc3072a23fde40d8afeb5d2db94
Valentin <finik001@gmail.com>
2019-11-18 18:58:44 +0300

    Выполнено задание  1 в модуле 4

---
+++ WFMS/04.Layouts/01_linearlaouy/src/main/java/ru/vetukov/wfms/a01_linearlaouy/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.vetukov.wfms.a01_linearlaouy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
