fad225cac1edffc01d466d7d263d11e1e4f01a65
Valentin <finik001@gmail.com>
2019-11-18 19:22:42 +0300

    Выполнено задание  2 в модуле 4

---
+++ WFMS/04.Layouts/02_tablelayout/src/main/java/ru/vetukov/wfms/a02_tablelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.vetukov.wfms.a02_tablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
