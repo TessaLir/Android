29d30bb74aea8dd943ef94741b54efb311ffd333
Valentin <finik001@gmail.com>
2019-12-15 14:37:48 +0300

    Самостоятельная работа по 20 модулю выполнена.

---
+++ AndroidKakPoNotam/ex-020-posterapp/src/main/java/ru/vetukov/test/ex_020_posterapp/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.vetukov.test.ex_020_posterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
