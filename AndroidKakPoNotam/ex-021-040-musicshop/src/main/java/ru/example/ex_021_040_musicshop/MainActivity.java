4a88381820fcc45b62762fcc3fd9c5c628928752
Valentin <finik001@gmail.com>
2019-12-17 11:02:52 +0300

    Добавлено взаимодействие со спинером, изменяется картинка и цена товара.

--- AndroidKakPoNotam/ex-021-040-musicshop/src/main/java/ru/example/ex_021_040_musicshop/MainActivity.java
+++ AndroidKakPoNotam/ex-021-040-musicshop/src/main/java/ru/example/ex_021_040_musicshop/MainActivity.java
@@ -4,15 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity
+        implements View.OnClickListener,
+                   AdapterView.OnItemSelectedListener {
 
     private static Button btnAdd;
     private static Button btnMinus;
-
+    private static Spinner spnAnimals;
     private static TextView tvItemCount;
+    private static TextView tvPrice;
+    private static ImageView ivItemImage;
+
+    private static ArrayList<String> spinnerArrayList;
+    private static HashMap animalPrice;
+
+    private ArrayAdapter spinnerAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +39,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnAdd = findViewById(R.id.main_btn_count_add);
         btnMinus = findViewById(R.id.main_btn_count_minus);
-
         tvItemCount = findViewById(R.id.main_tv_count);
+        spnAnimals = findViewById(R.id.main_spn_animals);
+        tvPrice = findViewById(R.id.main_tv_price);
+        ivItemImage = findViewById(R.id.main_iv_item);
+
+        spinnerArrayList = new ArrayList<>();
+        spinnerArrayList.add("Cats");
+        spinnerArrayList.add("Dogs");
+        spinnerArrayList.add("Rodents");
+
+        animalPrice = new HashMap<>();
+        animalPrice.put("Cats", 1500.0);
+        animalPrice.put("Dogs", 1000.0);
+        animalPrice.put("Rodents", 500.0);
+
+        spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerArrayList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnAnimals.setAdapter(spinnerAdapter);
 
         btnAdd.setOnClickListener(this);
         btnMinus.setOnClickListener(this);
+
+        spnAnimals.setOnItemSelectedListener(this);
 
     }
 
@@ -47,12 +83,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (flag == 1) {
             tvItemCount.setText(String.format("%s",++count));
             btnMinus.setClickable(true);
+            changePrice();
         } else {
             if (count > 0) {
                 tvItemCount.setText(String.format("%s",--count));
+                changePrice();
             } else {
                 btnMinus.setClickable(false);
             }
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        changePrice();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private static void changePrice() {
+        String animal = spnAnimals.getSelectedItem().toString();
+        double price = (double) animalPrice.get(animal);
+        int count = Integer.parseInt(tvItemCount.getText().toString());
+
+        switch (animal) {
+            case "Cats" :
+                ivItemImage.setImageResource(R.drawable.cats);
+                break;
+            case "Dogs" :
+                ivItemImage.setImageResource(R.drawable.dogs);
+                break;
+            case "Rodents" :
+                ivItemImage.setImageResource(R.drawable.rodents);
+                break;
+        }
+
+        tvPrice.setText(String.format("%s", price * count));
     }
 }
