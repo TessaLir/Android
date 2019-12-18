c2cded05a739d9c6f75ff8606db7d86dc9555ce4
Valentin <finik001@gmail.com>
2019-12-18 16:12:05 +0300

    Добавлена анимация , медленной смены картинки с Тома на Джери и в обратно

--- AndroidKakPoNotam/ex-047-063-tomandjerry/src/main/java/ru/skillbranch/ex_047_063_tomandjerry/MainActivity.java
+++ AndroidKakPoNotam/ex-047-063-tomandjerry/src/main/java/ru/skillbranch/ex_047_063_tomandjerry/MainActivity.java
@@ -3,12 +3,31 @@ package ru.skillbranch.ex_047_063_tomandjerry;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView ivTom;
+    private ImageView ivJerry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivTom = findViewById(R.id.main_iv_tom);
+        ivJerry = findViewById(R.id.main_iv_jerry);
+
+        ivTom.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        boolean flag = v.getAlpha() == 0;
+        ivTom.animate().alpha(flag ? 1 : 0).setDuration(1500);
+        ivJerry.animate().alpha(flag ? 0 : 1).setDuration(1500);
+    }
+
 }
