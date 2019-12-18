a6dd6c02230b8bd5d10a33018f959a780567c763
Valentin <finik001@gmail.com>
2019-12-18 16:41:15 +0300

    Повертели Тома и Джерри

--- AndroidKakPoNotam/ex-047-063-tomandjerry/src/main/java/ru/skillbranch/ex_047_063_tomandjerry/MainActivity.java
+++ AndroidKakPoNotam/ex-047-063-tomandjerry/src/main/java/ru/skillbranch/ex_047_063_tomandjerry/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final int TIME_SIZE = 3000;
+
     private ImageView ivTom;
     private ImageView ivJerry;
 
@@ -20,14 +22,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ivJerry = findViewById(R.id.main_iv_jerry);
 
         ivTom.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View v) {
         boolean flag = v.getAlpha() == 0;
-        ivTom.animate().alpha(flag ? 1 : 0).setDuration(1500);
-        ivJerry.animate().alpha(flag ? 0 : 1).setDuration(1500);
+        ivTom.animate().alpha(flag ? 1 : 0).setDuration(TIME_SIZE);
+        ivTom.animate().scaleX(flag ? 1 : 0).scaleY(flag ? 1 : 0).setDuration(TIME_SIZE);
+        ivTom.animate().rotation(flag ? -3600 : 3600).setDuration(TIME_SIZE);
+        ivJerry.animate().alpha(flag ? 0 : 1).setDuration(TIME_SIZE);
+        ivJerry.animate().scaleX(flag ? 0 : 1).scaleY(flag ? 0 : 1).setDuration(TIME_SIZE);
+        ivJerry.animate().rotation(flag ? 3600 : -3600).setDuration(TIME_SIZE);
     }
 
 }
